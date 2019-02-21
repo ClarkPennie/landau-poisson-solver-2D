43ce0d28deab4817abcea0b964d0f2ecb1f999f0
Clark Pennie <cpennie@math.utexas.edu>
2019-02-21 10:27:53 -0600

    Run a test with a field but no specular reflection
    This has shown that there is indeed an error in the specular reflection implemenetation.

--- jobscript.sh
+++ jobscript.sh
@@ -3,10 +3,10 @@
 #SBATCH -J LPsolver            # Job Name
 #SBATCH -A Boltzmann-spectral      # Charges run to <project_account_name>
 #SBATCH -o ./output_files/LPsolver.%j.out    # Name of the output and error file (%j expands to jobID) // only symbol % can be recoginized, others say & not
-#SBATCH -n 16     # Total number of mpi tasks requested
-#SBATCH -N 16       # The number of nodes. n/N tasks are launched on each node. used in conjunction with the -n option (above). Use this option to specify launching less than 16 tasks per node. 
-#SBATCH -p development     # Queue (partition) name  -- normal, largemem, development, gpu, etc.
-#SBATCH -t 00:30:00     # Run time (hh:mm:ss) 
+#SBATCH -n 64     # Total number of mpi tasks requested
+#SBATCH -N 64       # The number of nodes. n/N tasks are launched on each node. used in conjunction with the -n option (above). Use this option to specify launching less than 16 tasks per node. 
+#SBATCH -p normal     # Queue (partition) name  -- normal, largemem, development, gpu, etc.
+#SBATCH -t 03:00:00     # Run time (hh:mm:ss) 
 #SBATCH --mail-user=clark.pennie@utexas.edu     #Specify the email address to use for notifications.
 #SBATCH --mail-type=ALL       #Specify when user notifications are to be sent.
 
@@ -14,6 +14,6 @@ set -x
 export OMP_NUM_THREADS=68
 #export I_MPI_DEBUG=2
 
-ibrun tacc_affinity  ./source/LPsolver_2D
+ibrun tacc_affinity  ./source/LPsolver_2D_NoField
 # valgrind --leak-check=yes 
 #./fpl.out
